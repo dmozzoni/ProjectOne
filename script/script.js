798b7c23996790b804f5ae94fd1e36cd572f9a64
dmozzoni <cyric@hotmail.com>
2016-11-30 01:20:23 -0500

    google spreadsheet --> D3, added photos

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
-d3.csv("./data.csv", function(data) {
+let foo;
+
+
+d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQpA97t0qk19B00C617SeAF2eKZSJURLlNsy9b_UfgvUxti3Bw6ymc365TfoXHpQNfg7LDxVAYO_6-s/pub?gid=0&single=true&output=csv", function(data) {
+  foo = data;
   console.log(data[0]);
 });
